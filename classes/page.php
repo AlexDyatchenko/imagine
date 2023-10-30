d384b8cbaecc75fca8044acedd6583c40d29871b
Alex <Alex.Dyatchenko@gmail.com>
2023-10-30 22:22:32 +1000

    pages done

--- classes/page.php
+++ classes/page.php
@@ -5,8 +5,49 @@ require_once './classes/video.php';
 
 class Page
 {
+    private string $path = '';
+
     public function __construct(string $path)
     {
-        echo 'path = ' . $path . PHP_EOL;
+        $this->path = $path;
+        //echo 'path = ' . $path . PHP_EOL;
+    }
+
+    public function generate(): string
+    {
+        $output = file_get_contents($this->path . '/descroption.json');
+        //str_replace
+        $files = $this->getListOfFiles();
+        foreach ($files as $file) {
+            $output .= $this->generateOneBlock($file);
+        }
+        return $output;        
+    }
+
+
+    public function getListOfFiles(string $directory) : array
+    {
+        $files = array();
+        if (is_file($files)) {
+            $filesInDir = scandir($directory);
+            if ($filesInDir !== false) {                                
+                foreach ($filesInDir as $file) {
+                    // Check if it's a directory (not . or ..)
+                    if (is_file($directory . '/' . $file) && $file != "." && $file != "..") {
+                        $files[] = $file;
+                    }
+                }                        
+            } else {
+                echo "Failed to read the directory.";
+            }
+        } else {
+            echo "The directory does not exist.";
+        }
+        return $files;
+    }
+
+    public function generateOneBlock(string $file) : string
+    {
+        return $file;
     }
 }
--- index.php
+++ index.php
@@ -3,8 +3,20 @@
 namespace imagine;
 require_once './classes/outputProcessor.php';
 require_once './classes/page.php';
+require_once './classes/pages.php';
 
 
 $outputProcessor = new outputProcessor('index.html');
+
+
+$pages = new pages();
+$pagesFolders = $pages->getListOfFolders
+foreach ($pagesFolders as $folder) {
+    $page = new page($folder);
+    $videoBlock = $page->generate();
+    $outputProcessor->addToBody($videoBlock);    
+}
 $outputProcessor->addToBody(PHP_EOL. 'php works!');
 $outputProcessor->echo();
+
+function 
